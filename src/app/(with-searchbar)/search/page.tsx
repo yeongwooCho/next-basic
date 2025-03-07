ba0a575a4d534de80e2a628bc69f88985e90de91
yeongwoo <jyy0223@naver.com>
2025-03-07 21:12:00 +0900

    [feat] dynamic function으로 full route cache를 사용하지 못하는 search page의 경우 한번 검색 된 정보를 cache에 저장하는 방법으로 최적화

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -5,7 +5,10 @@ export default async function Page(
   {searchParams}: { searchParams: Promise<{ q?: string }> }
 ) {
   const {q} = await searchParams;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    {cache: "force-cache"}
+  );
   if (!response.ok) {
     return <div>오류가 발생했습니다.</div>;
   }
