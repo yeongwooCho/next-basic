0506b9c24f580f5789eec9b257ed44d090b288f5
yeongwoo <jyy0223@naver.com>
2025-03-08 17:39:17 +0900

    [chore] add skeleton UI

---
+++ src/components/skeleton/book.list-skeleton.tsx
@@ -0,0 +1,17 @@
+import BookItemSkeleton from "./book-item-skeleton";
+
+// export default function BookListSkeleton({ count }: { count: number }) {
+//   return (
+//     <>
+//       {Array.from({ length: count }).map((_, index) => (
+//         <BookItemSkeleton key={index} />
+//       ))}
+//     </>
+//   );
+// }
+
+export default function BookListSkeleton({ count }: { count: number }) {
+  return new Array(count)
+    .fill(0)
+    .map((_, idx) => <BookItemSkeleton key={`book-item-skeleton-${idx}`} />);
+}
