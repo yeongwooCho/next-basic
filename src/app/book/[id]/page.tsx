e4afcd5a730a0d2a6d1a9b835e4c2f6315af8e34
yeongwoo <jyy0223@naver.com>
2025-03-10 12:16:20 +0900

    [feat] server action을 통한 리뷰 작성 기능 추가

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,11 +1,11 @@
 import { notFound } from "next/navigation";
 import style from "./page.module.css";
 import { BookData } from "@/types";
+import { createReviewAction } from "@/actions/create-review.action";
 
 // generateStaticParams에 등록되지 않은 페이지의 경우 notFound page로 redirect 되는 dynamicParams 추가
 // page router의 fallback: false
 // export const dynamicParams = false;
-
 export async function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
@@ -13,11 +13,23 @@ export async function generateStaticParams() {
 export default async function Page({
   params,
 }: {
-  params: Promise<{ id: string | string[] }>;
+  // params: Promise<{ id: string | string[] }>;
+  params: Promise<{ id: string }>;
 }) {
   const paramsObj = await params;
+  const bookId = paramsObj.id as string;
+
+  return (
+    <div className={style.container}>
+      <BookDetail bookId={bookId} />
+      <ReviewEditor bookId={bookId} />
+    </div>
+  );
+}
+
+async function BookDetail({ bookId }: { bookId: string }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${paramsObj.id}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`
   );
 
   if (!response.ok) {
@@ -37,7 +49,7 @@ export default async function Page({
   }: BookData = await response.json();
 
   return (
-    <div className={style.container}>
+    <section>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -50,6 +62,19 @@ export default async function Page({
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
-    </div>
+    </section>
+  );
+}
+
+function ReviewEditor({ bookId }: { bookId: string }) {
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input name="bookId" value={bookId} hidden readOnly />
+        <input required name="content" type="text" placeholder="리뷰 내용" />
+        <input required name="author" placeholder="작성자" />
+        <button type="submit">작성하기</button>
+      </form>
+    </section>
   );
 }
